925ad4aca8ab3972b81855bc59814135a66184eb
MenuItem207 <70802541+MenuItem207@users.noreply.github.com>
2022-10-20 14:10:12 +0800

    refactored detector

--- detectorinator/detector.py
+++ detectorinator/detector.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-link = "INSERTLINKHERE"
-response = requests.request("GET", link)
-soup = BeautifulSoup(response.text, "lxml")
-print(soup)
-
 # takes in a link and extracts the relevant data
 def extract(link):
+    response = requests.request("GET", link)
+    soup = BeautifulSoup(response.text, "lxml")
+
     return {
         "url": link,
         "has_at": link_has_at(link),
@@ -25,3 +23,7 @@ def link_has_at(link):
 # returns 1 if has @ and 0 if not
 def link_has_dash(link):
     return 1 if "-" in link else 0
+
+
+link = "https://insert_link_here"
+print(extract(link))
